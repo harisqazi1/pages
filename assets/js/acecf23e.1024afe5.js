f416f5d490671d9ffb8baf95243ebba14ee54f83
Haris Qazi <harisfromcyber@codeberg.org>
2024-11-23 16:18:55 -0600

    Updating RSS Feed Format

---
+++ assets/js/acecf23e.1024afe5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknew_blog=self.webpackChunknew_blog||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Haris\'s Blog","authorsListPath":"/blog/authors"}')}}]);
